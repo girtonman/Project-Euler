fa3d8daf91319c336ff8928bf51cfcabb460fb6a
girtonman <girtonman@gmail.com>
2014-08-11 22:25:30 -0500

    Problems 6 - 7

---
+++ Solutions/Problem6.java
@@ -0,0 +1,13 @@
+public class Problem6 {
+   public static void main(String[] args) {
+      int max = 100;
+      int sumSquare = 0;
+      int squareSum = 0;
+      
+      for (int i = 1; i <= max; i++) {
+         sumSquare += Math.pow(i, 2);
+         squareSum += i;
+         System.out.println("1-" + i + ": " + (int)Math.pow(squareSum, 2) + " - " + sumSquare + " = " + ((int)Math.pow(squareSum, 2) - sumSquare));
+      }
+   }
+}
---
+++ Solutions/Problem7.java
@@ -0,0 +1,32 @@
+public class Problem7 {
+   public static void main(String[] args) {
+      int targetPrime = 10001;
+      int currentPrime = 0;
+      int prime = 0;
+      int number = 2;
+      int divisor = 2;
+      
+      while (currentPrime != targetPrime) {
+         //reach half way point, skip to next
+         if(divisor > (number / 2)) {
+            currentPrime++;
+            prime = number;
+            System.out.println("Prime #" + currentPrime + ": " + prime);
+            number++;
+            divisor = 2;
+         }
+         else {
+            //if it is compound, skip it
+            if(number%divisor == 0) {
+               number++;
+               divisor = 2;
+            }
+            //else increment divisor
+            else {
+               divisor++;
+            }
+         }
+         
+      }
+   }
+}
