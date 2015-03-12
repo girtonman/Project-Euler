4d0e659c71241427f79f1c2c155115f6e9ccf976
Scott Girton <girtonman@gmail.com>
2015-03-11 22:05:31 -0500

    Problem 14 rev 1

--- Solutions/Problem014.java
+++ Solutions/Problem014.java
@@ -1,6 +1,12 @@
+//rev 1
 //brute force
 //target answer = 837799
-//loop count = 132434271
+//old loop count = 132434271
+//new loop count = 6226258
+//~95% decrease in loop count
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Problem014 {
    private static long loopCounter = 0;
@@ -10,19 +16,27 @@ public class Problem014 {
       int longestChainStart = 0;
       long loopCounter = 0;
       int startingLimit = 1000000;
+      Map<String, String> collatzCache = new HashMap<String, String>();
       
       for(int startingNumber = 1; startingNumber < startingLimit; startingNumber++) {
          long number = startingNumber;
          int chainCount = 1;
          
          while(number != 1){
-            loopCounter++;    
+            loopCounter++;
+            String existing = collatzCache.get("" + number);
+            if(existing != null) {
+               chainCount += Integer.parseInt(existing) - 1;
+               break;
+            }
             if(number % 2 == 0)
                number = number / 2;
             else
                number = 3*(number) + 1;
             chainCount++;
          }
+         
+         collatzCache.put("" + startingNumber, "" + chainCount);
          if(number == 1)
             loopCounter++;
          if(chainCount > longestChain) {
